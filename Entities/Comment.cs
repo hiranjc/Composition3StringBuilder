48dfe9b6db31e65dd5bf38a8395b53d9ba303193
Hiran Ferreti Baccos <hiranjc1@outlook.com>
2025-09-18 18:29:58 -0300

    first commit

---
+++ Entities/Comment.cs
@@ -0,0 +1,14 @@
+﻿namespace Composition3StringBuilder.Entities
+{
+    internal class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment() { }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
